1110471d5bf83302cef71c157f4f432c0dc77bd8
tacorabane <blackbomba32@hotmail.fr>
2014-10-28 09:33:58 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
-  return 0;
+#include "librairy.h"
+
+int main(int argc, char **argv)
+{
+    if(argc < 4)
+    {
+        printf("Not enough arguments\n");
+        return 1;
+    }
+
+    if(!is_well_formated(argv[2]))
+    {
+        printf("The first input file is not well-formated\n");
+        return 2;
+    }
+
+    if(!is_well_formated(argv[3]))
+    {
+        printf("The second input file is not well-formated\n");
+        return 2;
+    }
+
+    Attributs *attributs;
+    AttributsRepere *attributs_repere;
+    addAttr(attributs, attributs_repere, "div");
+
+    while(attributs_repere != NULL)
+    {
+        printf("%p -> %s", attributs, attributs->attr);
+        attributs = attributs->next;
+    }
+
+    return 0;
 }
